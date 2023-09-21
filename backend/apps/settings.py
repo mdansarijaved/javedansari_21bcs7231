4691d008eefa1ba36a4d12390da76d3431a375e1
pallabez <pallabsonowal999@gmail.com>
2023-09-21 20:21:47 +0530

    add static files

--- backend/apps/settings.py
+++ backend/apps/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,6 +27,9 @@ SECRET_KEY = 'django-insecure-=6n@2sbhhy^ew_(%*9!3u+whaybka_m!*)a9@+_xng9dtxs%ph
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Application definition
