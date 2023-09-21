59e81286ded88af32e020d264e8b08ecd622dce8
Javed Ansari <javedans2003@gmail.com>
2023-09-21 21:14:34 +0530

    login form

--- frontend/src/app/blog/page.jsx
+++ frontend/src/app/blog/page.jsx
@@ -1,33 +1,55 @@
+'use client'
 import React from 'react'
+import { useState, useEffect } from 'react'
 import './page.css'
 import ReactMarkdown from 'react-markdown'
-const markdown = `Just a link: https://reactjs.com.`
+const markdown = `##Just a link: https://reactjs.com.`
 
 const Create = () => {
+    const [text, setText] = useState('')
+
     return (
         <>
-        <div className='main'>
-            <nav className='navCreate'>
-                <h1 className='logo'>YourBlog</h1>
-                <h2 className='createTag'>Wrtie your story.</h2>
-                <div className='buttons'>
-                    <button className='blogButton'>
-                        Publish
-                    </button>
-                    <button className='blogButton previewButton'>
-                        Preview
-                    </button>
-                </div>
-            </nav>
+            <div className='main'>
+                <nav className='navCreate'>
+                    <h1 className='logo'>YourBlog</h1>
+                    <h2 className='createTag'>Wrtie your story.</h2>
+                    <div className='buttons'>
+                        <button className='blogButton'>
+                            Publish
+                        </button>
+                        <button className='blogButton previewButton'>
+                            Preview
+                        </button>
+                    </div>
+                </nav>
 
-            <main>
-            <ReactMarkdown children={markdown} />
+                <main>
+                    <textarea name="text-area" id="" cols="30" rows="10"></textarea>
+                    {/* <PageContent /> */}
                 </main>
-        </div>
+            </div>
 
-        
+
         </>
     )
 }
 
 export default Create
+
+
+const PageContent = () => {
+    const [content, setContent] = useState('')
+
+    useEffect(() => {
+        fetch("README.md")
+            .then((res) => res.text())
+            .then((text) => setContent(text))
+    }, [])
+
+    return (
+        <div>
+            <ReactMarkdown children={content} />
+        </div>
+    )
+}
--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -1,36 +1,42 @@
-import React from 'react'
-import './login.css'
+import React from "react";
+import "./login.css";
 
 const page = () => {
   return (
     <>
-    <section>
-      <div>
-        <h1>Hello</h1>
-        <p>Sign in to <a href ="#">My Blog</a> or <a href="#">Create an account</a></p>
-        <input type="email" placeholder='Email or Username'/>
-        <br />
-        <input type="password" placeholder='Password' />
-        <div>
-        <button>Continue</button>
-        </div>
-        <div>
-          <button>Continue with Facebook</button>
-        </div>
-        <div>
-          <button>Continue with Google</button>
-        </div>
-        <div>
-          <button>Contiue with Apple</button>
-        </div>
-        <div>
-          <input type="checkbox" name="" id="" />
-        <span> Stay signed in</span>
-        </div>
-        </div>
-    </section>
-    </>
-  )
-}
+      <section className="main">
+        <div className="main-container">
 
-export default page
+          <h1 className="intro-text">Hello 👋</h1>
+          <p className="textcontent">
+            Sign in to your account
+          </p>
+          <div className="inputdiv">
+            <input type="email" placeholder="Email or Username" className="input" />
+          </div>
+          <div className="inputdiv">
+            <input type="password" placeholder="Password" className="input" />
+          </div>
+          <div>
+            <button className="buttons">Continue</button>
+          </div>
+          <p>OR</p>
+          <div>
+            <button className="buttons">Continue with Facebook</button>
+          </div>
+          <div>
+            <button className="buttons">Continue with Google</button>
+          </div>
+
+          <div className="inputradio">
+            <input type="checkbox" name="" id="" />
+            <span> Stay signed in</span>
+          </div>
+
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default page;
