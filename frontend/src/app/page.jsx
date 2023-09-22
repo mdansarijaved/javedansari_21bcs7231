15bf47a951b0b1311a2509cf538785c4757b2813
Javed Ansari <javedans2003@gmail.com>
2023-09-23 00:52:33 +0530

    eww colors

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -19,17 +19,17 @@ export default function Home() {
           ))}
         </div>
         <div className="anotherdiv">
-          <div className="svg-container text2">
+          <div className="svg-container ">
             <div className="text-svg-container ">
             <h3 className="">Let the world know your story.</h3>
             <p>Your unique voice and perspective can spark change and inspire others. Share your story with the world through a blog.</p>
             </div>
           <Image src={'/images/16.svg'} width={200} height={200} alt="svg" className="rotate-svg" />
           </div>
-          <div className="blogText text2">
+          <div className="blogText">
             <p className="blogparaText">Blogging is a dynamic and fulfilling activity that offers numerous advantages. Firstly, it provides an outlet for self-expression and creativity.</p>
             </div>
-            <div className="blogText text2">
+            <div className="blogText ">
 
               <p className="blogparaText">Furthermore, blogging is a powerful tool for personal development. It enhances your writing abilities, critical thinking skills, and research capabilities.</p>
             </div>
