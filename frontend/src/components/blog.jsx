39eb7365a504ec14ea8395b45a28231d71114ffb
pallab <okay>
2023-09-22 20:52:15 +0530

    add link to blog title

--- frontend/src/components/blog.jsx
+++ frontend/src/components/blog.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import styles from "./blogcontent.module.css";
 import CreatedBy from "./CreatedBy";
+import Link from "next/link";
 const Blog = ({ blog }) => {
   const { title, content, create_by: uid } = blog;
 
@@ -9,9 +10,10 @@ const Blog = ({ blog }) => {
     <div className={styles.main}>
       <CreatedBy uid={uid} />
       <div className={styles.blogbody}>
-
-      <h1 className={styles.tittle}>{title}</h1>
-      <ReactMarkdown className={styles.content}>{content}</ReactMarkdown>
+        <Link href={`/blog/${blog.id}`}>
+          <h1 className={styles.tittle}>{title}</h1>
+        </Link>
+        <ReactMarkdown className={styles.content}>{content}</ReactMarkdown>
       </div>
     </div>
   );
