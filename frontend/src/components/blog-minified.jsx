af175f2f0971824886d1a487e08020ea0281b100
Javed Ansari <javedans2003@gmail.com>
2023-09-22 20:44:25 +0530

    card on home

--- frontend/src/components/blog-minified.jsx
+++ frontend/src/components/blog-minified.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import CreatedBy from './CreatedBy'
 
 const BlogMinified = ({ blog }) => {
   return (
     <a className="card" href={`/blog/${blog.id}`}>
+      <CreatedBy uid={blog.create_by} />
       <h1 className="card-tittle">
         {blog.title}
       </h1>
