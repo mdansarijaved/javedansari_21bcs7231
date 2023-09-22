c27e439685dd75913e99ce00f70e88b612d8887e
Javed Ansari <javedans2003@gmail.com>
2023-09-22 20:33:51 +0530

    date component

--- frontend/src/components/blog.jsx
+++ frontend/src/components/blog.jsx
@@ -1,35 +1,21 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import styles from "./blogcontent.module.css";
-import { ThumbsUp, ThumbsDown } from "lucide-react";
-import Image from "next/image";
+import CreatedBy from "./CreatedBy";
 const Blog = ({ blog }) => {
   const { title, content, create_by: uid } = blog;
-  const time = new Date().getDate();
+
   return (
     <div className={styles.main}>
-      <div className={styles.profile}>
-        <div className={styles.imagediv}>
-          <Image
-            src={"/images/hero.jpg"}
-            width={200}
-            height={200}
-            alt="profile"
-          />
-        </div>
-        <p className={styles.uid}>{uid} <span className={styles.dot}> . </span> <span className={styles.time}>{time}Sept/2023</span></p>
-      </div>
+      <CreatedBy uid={uid} />
       <div className={styles.blogbody}>
 
       <h1 className={styles.tittle}>{title}</h1>
       <ReactMarkdown className={styles.content}>{content}</ReactMarkdown>
       </div>
-      {/* <div className={styles.reaction}>
-      <ThumbsUp />
-      <ThumbsDown />
-      </div> */}
     </div>
   );
 };
 
 export default Blog;
+
