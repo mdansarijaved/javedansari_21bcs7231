832cc70f6881b4c3bf64c6824d01958ee82b253c
Javed Ansari <javedans2003@gmail.com>
2023-09-22 19:43:25 +0530

    blog

--- frontend/src/app/blog/page.jsx
+++ frontend/src/app/blog/page.jsx
@@ -2,17 +2,20 @@
 import { useGetPostsQuery } from "@/store/posts"
 import Blog from "@/components/blog";
 import styles from './blogs.module.css'
+import Nav from "@/components/nav-bar";
+
 
 export default function Blogs() {
     const { data: blogs, isLoading } = useGetPostsQuery();
     
     if (isLoading) return <div>Loading...</div>
+    console.log(blogs)
 
     // Assuming 'blogs' is an array of posts, map over it to render each post
     return (
         <div className={styles.main}>
             {blogs.map((post) => (
-                <Blog key={post.id} title={post.title} content={post.content} />
+                <Blog key={post.id} title={post.title} content={post.content} uid={post.create_by} />
             ))}
         </div>
     )
--- frontend/src/components/blog.jsx
+++ frontend/src/components/blog.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
-import ReactMarkdown from 'react-markdown'
-import styles from './blogcontent.module.css'
-const Blog = ({Title , content}) => {
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import styles from "./blogcontent.module.css";
+import { ThumbsUp, ThumbsDown } from "lucide-react";
+import Image from "next/image";
+const Blog = ({ title, content, uid }) => {
+  const time = new Date().getDate();
   return (
     <div className={styles.main}>
-        <h1>{Title}</h1>
-        <ReactMarkdown>{content}</ReactMarkdown>
-    </div>
-  )
-}
+      <div className={styles.profile}>
+        <div className={styles.imagediv}>
+          <Image
+            src={"/images/hero.jpg"}
+            width={200}
+            height={200}
+            alt="profile"
+          />
+        </div>
+        <p className={styles.uid}>{uid} <span className={styles.dot}> . </span> <span className={styles.time}>{time}Sept/2023</span></p>
+      </div>
+      <div className={styles.blogbody}>
 
-export default Blog
+      <h1 className={styles.tittle}>{title}</h1>
+      <ReactMarkdown className={styles.content}>{content}</ReactMarkdown>
+      </div>
+      {/* <div className={styles.reaction}>
+      <ThumbsUp />
+      <ThumbsDown />
+      </div> */}
+    </div>
+  );
+};
+
+export default Blog;
