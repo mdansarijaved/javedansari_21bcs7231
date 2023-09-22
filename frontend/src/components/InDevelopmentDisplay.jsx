06c7fadfe7c053ba0a20fdfa6419fc994040d837
pallabez <pallabsonowal999@gmail.com>
2023-09-22 19:19:38 +0530

    scaffold inDevelopmentDisplay component

---
+++ frontend/src/components/InDevelopmentDisplay.jsx
@@ -0,0 +1,14 @@
+import './inDevelopmentDisplay.css';
+
+export default function InDevelopmentDisplay() {
+    return (
+        <div className="in-development__container">
+            <div className="in-development__text in-development__text--upper">
+
+            </div>
+            <div className="in-development__text in-development__text--lower">
+
+            </div>
+        </div>
+    )
+}
