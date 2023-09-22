c95f98a44282e32257bb86d85f988b2f042c8562
pallab <okay>
2023-09-23 00:55:20 +0530

    fix delete in home page

--- frontend/src/components/BlogMenu.jsx
+++ frontend/src/components/BlogMenu.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './blogMenu.css'
 import { useDeletePostMutation } from '@/store/posts';
 import { useUser } from '@/store/user';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 export default function BlogMenu({ blog }) {
     const { create_by: uid, id } = blog;
@@ -24,7 +24,13 @@ export default function BlogMenu({ blog }) {
             {showDropdown ?
                 <div className="menu__dropdown">
                     <button
-                        onClick={() => deleteBlog({ id }) && router.push('/')}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            e.stopPropagation();
+
+                            deleteBlog({ id })
+                            router.push('/')
+                        }}
                         className={`menu__dropdown-option`}
                     >Delete</button>
                 </div> :
