cadaea11ee72377a72ae54b73057ed1574af804f
pallab <okay>
2023-09-22 20:21:53 +0530

    add blog page by id

--- frontend/src/app/blog/[slug]/page.jsx
+++ frontend/src/app/blog/[slug]/page.jsx
@@ -1,5 +1,14 @@
+'use client'
+import Blog from "@/components/blog"
+import { useGetPostByIdQuery } from "@/store/posts"
+
 export default function Page({ params }) {
+    const { data: blog, isError, isLoading } = useGetPostByIdQuery({ id: params.slug })
+    
+    if (isError) return <div>Not Found</div>
+    if (isLoading) return <div>Loading...</div>
+
     return (
-        <div>My Post: {params.slug}</div>
+        <Blog blog={blog}/>
     )
 }
